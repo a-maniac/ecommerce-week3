78e54aca99856a0fbf2e53d819d3bc07e9cab24d
amanjoshi <ajoshi373@gmail.com>
2024-10-02 15:01:56 +0530

    added controller

--- inventory-service/src/main/java/com/amaniac/ecommerce/inventory_service/controller/ProductController.java
+++ inventory-service/src/main/java/com/amaniac/ecommerce/inventory_service/controller/ProductController.java
@@ -1,4 +1,33 @@
 package com.amaniac.ecommerce.inventory_service.controller;
 
+import com.amaniac.ecommerce.inventory_service.dto.ProductDto;
+import com.amaniac.ecommerce.inventory_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> findAllInventory(){
+        List<ProductDto>inventories= productService.getAllInventory();
+        return new ResponseEntity<>(inventories, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getInventoryById(@PathVariable Long id){
+        ProductDto inventory= productService.getProductById(id);
+        return new ResponseEntity<>(inventory, HttpStatus.OK);
+    }
 }
